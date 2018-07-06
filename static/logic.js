3b5285af4efcdeca809956a7e733e449886edb99
ehyoder <ehyoder@gmail.com>
2018-07-06 16:54:31 -0400

    updates

--- static/logic.js
+++ static/logic.js
@@ -2,10 +2,12 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_hour.geojson"
 
 // // Perform a GET request to the query URL
-// d3.json(queryUrl, function(data) {
+d3.json(queryUrl, function(data) {
+      console.log(queryUrl)
+
 //   // Once we get a response, send the data.features object to the createFeatures function
-//   createFeatures(data.features);
-// });
+  createFeatures(data.features);
+});
 
 // function createFeatures(earthquakeData) {
 
