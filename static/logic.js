ef5234b5b1175e2287d0a11592a4d3cf8e208e83
ehyoder <ehyoder@gmail.com>
2018-07-06 18:03:32 -0400

    updates

--- static/logic.js
+++ static/logic.js
@@ -1,9 +1,6 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
-// // Store our API endpoint inside queryUrl
-// var queryUrl = "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-//   "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+// 2.5 Magnitude+ for the last 30 days is chosen dataset
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geojson"
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
